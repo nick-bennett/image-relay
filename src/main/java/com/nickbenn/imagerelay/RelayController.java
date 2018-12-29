b5ff77baa72cbf557657c4d9daf120725123de32
Nick Bennett <nick@nickbenn.com>
2018-12-28 22:13:24 -0700

    Remove unused import.

--- src/main/java/com/nickbenn/imagerelay/RelayController.java
+++ src/main/java/com/nickbenn/imagerelay/RelayController.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
